e5cfe8d2f7df45f8b65955bbccab99d96ff518ef
Elizenai Silva <elizenai.silva@hotmail.com>
2022-05-05 15:08:17 -0300

    docs: config react
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by:gabrieladmr@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1> Hello World</h1>
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
