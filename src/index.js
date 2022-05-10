a8a6dff33e57d88787915977f4bc9bec31c9b527
MariaLuizaSantana <costamalu123@outlook.com>
2022-05-10 17:19:25 -0300

    iniciando as organizações de pastas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+//import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom'
+import Login from './login';
+import Oi from './waiter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); 
+*/
+
+ReactDom.render(
+  <>
+    <Login/>
+    <Oi/>
+  </>,
+  document.getElementById('root')
+)
---
+++ src/login/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+function Login() {
+  return (
+    <header>
+      <h1>Burger Queen Service</h1>
+    </header>
+  );
+}
+
+export default Login;
