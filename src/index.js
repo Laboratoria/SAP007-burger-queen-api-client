eb73936f7385cc2bcacdc7df35d5075ee51927cc
Layssa <oilayssaa@gmail.com>
2022-05-09 22:03:57 -0300

    install npm i react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AllRoute from './route.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AllRoute/>
+  </React.StrictMode>
+);
+
+
+
+
+/*import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +29,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const history = useNavigate();
+
+  return (
+    <div className='main'>
+      <form className='login-page'>
+        <h2 className='form-title'>Colaborador, faça seu login:</h2>
+        <label className='form-labels'>Email</label>
+        <input className='form-input' type='email' name='email' autoComplete='off'/>
+        <label className='form-labels'>Senha</label>
+        <input className='form-input' type='password' name='password' />
+        <button className='form-button draw' type='submit'>Logar</button>
+        <p className='new-user'>Não possui cadastro?</p>
+        <button className='form-button draw' onClick={() => { history('/signup') }}>Cadastre-se</button>
+      </form>
+    </div>
+  );  
+};
+
+export default Login;
+
+// 'react-router-dom' - instalado com o cmd -> npm i react-router-dom
