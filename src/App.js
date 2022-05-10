abf0d3dc8219ffe78137788dd4f0d1979cfd92df
Amanda Gusmão <engamandagusmao@gmail.com>
2022-05-10 15:22:24 -0300

    Primeira Estilização

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import logo from './logo_bq.png';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <h1> hello world</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          SERVICE SYSTEM
+        </p>
+      </header>
     </div>
   );
 }
