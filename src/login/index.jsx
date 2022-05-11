7e7f168cb0c70710ce5f4f5efd62754623326b66
Amanda Gusmão <engamandagusmao@gmail.com>
2022-05-11 01:46:16 -0300

    Organizando CSS

--- src/login/index.jsx
+++ src/login/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from'../img/logo_bq.png';
 import './login.css';
+import '../App.css'
 import Input from './input';
 import Button from './button';
 import Footer from './footer';
