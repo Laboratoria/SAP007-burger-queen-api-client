de56cec864796a54c5a1008f4c1c89d28c22560e
Marione-Pereira <mari.tainara@gmail.com>
2022-05-11 20:58:53 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> 8812185a42e78f00ef5067dc09a16cc0bc86ac99
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <h1>HelloWorld</h1>
->>>>>>> 8812185a42e78f00ef5067dc09a16cc0bc86ac99
     </div>
   );
 }
