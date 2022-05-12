576a3c63b3981410e5421abcff2ecd53e380b548
Elizenai Silva <elizenai.silva@hotmail.com>
2022-05-11 21:43:07 -0300

    feat: creat register page
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { Layout } from "../../components/Layout";
+
+
+export const Register = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    // const [name, setName] = useState(""); 
+    
+    return (
+    <Layout>
+      <Input
+      placeholder= "Digite seu email"
+      type= "email"
+      value= {email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+      placeholder= "*******"
+      type= "password"
+      value= {password}
+      onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button
+      className="botaozinho"
+      btnText="oi brasil"
+      />
+    </Layout>
+   ); 
+
+};
