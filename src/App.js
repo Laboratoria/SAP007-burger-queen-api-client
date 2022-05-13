235f54405b992e432e2a795c6a3adb0db6af428e
Fe-Monteiro <fe.199nanda@gmail.com>
2022-05-13 19:20:20 -0300

    Implementando input na tela login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Footer } from "./components/Footer.js"
+import Form from "./components/Form.js"
 
 function App() {
   return (
     <div className="App">
       <h1>Burger</h1>
+      < Form />
       < Footer />
     </div>
   )
