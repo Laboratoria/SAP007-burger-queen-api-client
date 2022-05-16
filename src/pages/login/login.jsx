bc6742d2299ada1427605dde3e95f5339aafe3b5
Layssa <oilayssaa@gmail.com>
2022-05-16 15:24:07 -0300

    botao submit e condições para msg de erro

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -36,3 +36,31 @@ const Login = () => {
       [name]: value,
     });
   };
+
+  // A função handleSubmit() faz duas coisas:
+  // 1. Registra o valor atual do elemento de entrada(input) sempre que o formulário for enviado;
+  // 2. Impede o comportamento padrão do formulário HTML de navegar para uma nova página. 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    (defineErrors(errorsMessages(values)));
+
+    signIn(values.email, values.password).then((response) => {
+      if (response.code === 400) {
+        defineShowModalErrors(true);
+      } else {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('id', response.id);
+
+        if (response.role === "hall") {
+          history.push('/hall')
+        }
+        else if (response.role === "kitchen") {
+          history.push('/kitchen')
+        }
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  };
