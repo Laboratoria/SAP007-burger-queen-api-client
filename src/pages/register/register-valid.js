8d284e3c98e87b9ed658660540fbd813b26a1e90
Layssa <oilayssaa@gmail.com>
2022-05-16 15:38:24 -0300

    condicoes para msg de erro no register

--- src/pages/register/register-valid.js
+++ src/pages/register/register-valid.js
@@ -9,4 +9,16 @@ export const errorsMessages = (values) => {
     errors.email = 'Preencha seu email';
   } else if (!/\S+@\S+\.\S+/.test(values.email)) {
     errors.email = 'Ops... Email inválido. Tente novamente';
-  
+  }
+  if (!values.password) {
+    errors.password = 'Preencha sua senha';
+  } else if (values.password.length < 6) {
+    errors.password = 'A senha deve conter no mínimo 6 caracteres';
+  }
+  if (!values.role) {
+    errors.role = 'Selecione sua função!';
+  }
+  return errors;
+}
+
+export default errorsMessages;
