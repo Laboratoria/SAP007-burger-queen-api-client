729aaf65810dd2329cd063fe6333ee127e3cfb57
Keila Hadama <hadamakeila@gmail.com>
2022-05-16 18:26:01 -0300

    inicio pag cadastro

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+const Checkbox = ({ label, value, onChange }) => {
+  return (
+    <label>
+      <input type="checkbox" checked={value} onChange={onChange} />
+      {label}
+    </label>
+  );
+};
+
+export default Checkbox;
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,32 @@
+import Input from "../../components/Inputs";
+import Button from "../../components/Button";
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import Footer from "../../components/Footer";
+import "./styles.modules.css";
+import Checkbox from "../../components/Checkbox";
+
 function Register() {
     return (
         <>
-            <h1>mudou</h1>
+            <Header />
+            <section className="container-register">
+                <aside className="container-banner">
+                    <Banner className="banner" />
+                </aside>
+                <form className="register">
+                    <Input className="user-name" type="text" placeholder="Nome" />
+                    <Input className="email" type="email" placeholder="email@email.com" />
+                    <Input className="password" type="password" placeholder="senha" />
+                    <div className="checkbox">
+                        <Checkbox className="user-kitchen" type="checkbox" label="Atendimento"/>
+                        <Checkbox className="user-waitress" type="checkbox" label="Cozinha"/>
+                    </div>
+                    <Button className="button-register" type="button" nome="Cadastrar" />
+                    <a className="login-link" href="/"> Ou entre aqui</a>
+                </form>
+            </section>
+            <Footer />
         </>
     );
 };
