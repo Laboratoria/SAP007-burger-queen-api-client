ece8e0705bbb1a0734c8c144c2a543f8d3773164
Marione-Pereira <mari.tainara@gmail.com>
2022-05-16 13:48:51 -0300

    comecando com os userState
    Co-authored-by: Marione-Tainara <mari.tainara@gmail.com>
    Co-authored-by: moreirathais <thaismoreirajesusbonalume>

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,16 +1,18 @@
 import InputEmail from "../components/InputEmail";
 import InputPassword from "../components/InputPassword";
 import Button from "../components/Button";
-import Footer from "../components/Footer"
-import {useState} from "react"
-function createUser(e){
-    e.preventDefault();
-    console.log(`Criando Usuário com ${email} e ${password}`)
-}
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+import Footer from "../components/Footer";
+import {useState} from "react";
 
 function Register() {
+  const [email, setEmail] = useState ("");
+  const [password, setPassword] = useState("");
+
+  function createUser(e){
+      e.preventDefault();
+      console.log(`Criando Usuário com ${email} e ${password}`)
+  }
+  
   return (
     <form onSubmit={createUser}className="App">
       <InputEmail htmlFor="email" value="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
