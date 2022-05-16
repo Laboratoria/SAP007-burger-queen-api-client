828ded6a4e55549bc4527e0d79de145c589f634f
Maryssun <maryssol.dayanne@gmail.com>
2022-05-15 22:36:19 -0300

    feat:refatoração do css global

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Login';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.style.module.css";
+// import App from "./App";
+import { Router } from "./routes";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />        
+    <Router />
   </React.StrictMode>
 );
