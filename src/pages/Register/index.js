f1162f0d66a6001c0dfdf883b5323e37fc7b3370
Keila Hadama <hadamakeila@gmail.com>
2022-05-16 21:58:29 -0300

    lendo valor dos inputs e  criando user

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -5,8 +5,41 @@ import Banner from "../../components/Banner";
 import Footer from "../../components/Footer";
 import "./styles.modules.css";
 import Checkbox from "../../components/Checkbox";
+import {useState} from 'react'
 
 function Register() {
+    function registerUser(e) {
+        e.preventDefault()
+        console.log("clicou")
+        console.log(name)
+        console.log(email)
+        console.log(password)
+        // console.log(user)
+        const body = {
+            "name": name,
+            "email": email,
+            "password": password,
+            "role": "sample role",
+            "restaurant": "sample restaurant"
+          }
+        const callApi = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        }
+        fetch("https://lab-api-bq.herokuapp.com/users", callApi)
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data)
+        })
+
+    }
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState()
+    // const [user, setUser] = useState({})
+    
     return (
         <>
             <Header />
@@ -14,15 +47,15 @@ function Register() {
                 <aside className="container-banner">
                     <Banner className="banner" />
                 </aside>
-                <form className="register">
-                    <Input className="user-name" type="text" placeholder="Nome" />
-                    <Input className="email" type="email" placeholder="email@email.com" />
-                    <Input className="password" type="password" placeholder="senha" />
+                <form className="register" onSubmit={registerUser}> 
+                    <Input className="user-name" type="text" placeholder="Nome" name="name" onChange={(e) => setName(e.target.value)} />
+                    <Input className="email" type="email" placeholder="email@email.com" name="email" onChange={(e) => setEmail(e.target.value)}/>
+                    <Input className="password" type="password" placeholder="senha" name="password" onChange={(e) => setPassword(e.target.value)}/>
                     <div className="checkbox">
                         <Checkbox className="user-kitchen" type="checkbox" label="Atendimento"/>
                         <Checkbox className="user-waitress" type="checkbox" label="Cozinha"/>
                     </div>
-                    <Button className="button-register" type="button" nome="Cadastrar" />
+                    <Button className="button-register" type="submit" nome="Cadastrar"/>
                     <a className="login-link" href="/"> Ou entre aqui</a>
                 </form>
             </section>
