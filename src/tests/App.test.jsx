f58781b1ae4a4ca0a53ea9172c7c8e441a4672a6
moreirathais <thais_mjs@hotmail.com>
2022-05-22 20:37:55 -0300

    Inicio ajustes responsivo e testes componentes
    Co-authored-by: Marione-Tainara <mari.tainara@gmail.com>
    Co-authored-by: moreirathais <thaismoreirajesusbonalume>

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,14 +1,39 @@
 import '@testing-library/jest-dom';
-import { screen } from '@testing-library/react';
-import App from '../App';
+import { screen, render } from '@testing-library/react';
 import Button from '../components/Button';
-
+import Footer from '../components/Footer';
+import Input from '../components/Input';
+import Logo from '../components/Logo';
 
 describe('<Button />', () => {
     it('Renderiza um botão corretamente', () => {
-      renderWithTheme(<Button>conteudo</Button>);
-      const button = screen.getByRole('button');
+      render(<Button text="text"/>);
+      const text = "Logar"
+      const button = screen.getByText(text);
       expect(button).toBeInTheDocument();
-      expect(button).toMatchSnapshot();
     });
-  }
+  })
+
+  describe('<Footer />', () => {
+    it('Renderiza um rodapé corretamente', () => {
+      render(<Footer/>);
+      const footer= screen.getByText("footer");
+      expect(footer).toBeInTheDocument();
+    });
+  })
+
+  describe('<Input />', () => {
+  it('Renderiza um input corretamente', () => {
+    render(<Input placeholder="placeholder"/>);
+    const input= screen.getAllByPlaceholderText("placeholder");
+    expect(input).toBeInTheDocument();
+  });
+})
+
+  describe('<Logo />', () => {
+    it('Renderiza a logo corretamente', () => {
+      render(<Logo/>);
+      const logo= screen.getByRole("logo");
+      expect(logo).toBeInTheDocument();
+    });
+  })
