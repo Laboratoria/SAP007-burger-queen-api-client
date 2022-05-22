cc9c5919276bad6c56b54bfec15c1b153022db97
Layssa <oilayssaa@gmail.com>
2022-05-22 11:41:29 -0300

    pagina de login com os components

---
+++ src/components/style-html-elements/elements-style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DefaultParagraph = styled.p`
+  color: #FFAE1A;
+  margin-top: 1em;
+`;
+
+export const LoginRegisterParagraph = styled(DefaultParagraph)`
+  color: #531CB3;
+`;
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,3 +1,58 @@
-import errosMessages from "./login-validation";
 import useForm from "./useForm";
-import { Link } from 'react-router-dom';
+import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
+import ErrorsMessage from "./ValidationForm";
+import { Link } from "react-router-dom";
+import {
+  LinkStyle,
+  DefaultParagraph,
+} from "../../components/style-html-elements/elements-style";
+import { LoginRegisterParagraph } from "../../components/style-html-elements/elements-style";
+
+const LoginUser = () => {
+  const { handleChange, handleSubmit, errors } = useForm(ErrorsMessage);
+
+  return (
+    <>
+      <section>
+        <form
+          className="container container-login"
+          action=""
+          onSubmit={handleSubmit}
+        >
+          <Input
+            label="Email:"
+            id="email"
+            name="email"
+            type="text"
+            onChange={handleChange}
+            placeholder="Digite seu email"
+          />
+          <LoginRegisterParagraph>
+            {errors.email && errors.email}
+          </LoginRegisterParagraph>
+
+          <Input
+            label="Senha:"
+            id="password"
+            name="password"
+            type="password"
+            onChange={handleChange}
+            placeholder="Digite sua senha"
+          />
+          <LoginRegisterParagraph>
+            {errors.password && errors.password}
+          </LoginRegisterParagraph>
+
+          <Button>Entrar</Button>
+          <DefaultParagraph>Ainda não tem uma conta?</DefaultParagraph>
+          <Link to="/register">
+            <LinkStyle>Registre-se aqui</LinkStyle>
+          </Link>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default LoginUser;
