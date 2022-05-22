6e848ee42f26fea911d59917972e7ed9924ef7f0
Layssa <oilayssaa@gmail.com>
2022-05-22 11:00:08 -0300

    errosMessages

--- src/pages/login/useForm.jsx
+++ src/pages/login/useForm.jsx
@@ -1,42 +1,77 @@
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate } from "react-router-dom";
+import { LoginWithEmailPassword } from "../../services/api-auth";
 
 const useForm = (validation) => {
   localStorage.clear();
-  const [messageError, setMessageError] = useState('');
+  const [messageError, setMessageError] = useState("");
 
   const [values, setValues] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const [errors, setErrors] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     const updatedValue = {
       ...values,
       [name]: value,
-    }
+    };
     setValues(updatedValue);
 
-    const validations = validation(updatedValue)
+    const validations = validation(updatedValue);
     setErrors(validations);
-    
   };
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const routerHall = () => {
-    navigate('/hall')
-  }
+    navigate("/hall");
+  };
   const routerKitchen = () => {
-    navigate('/kitchen')
-  }
+    navigate("/kitchen");
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  
+    const validations = validation(values);
+    setErrors(validations);
+
+    if (validations.ok) {
+      LoginWithEmailPassword(values.email, values.password)
+        .then((response) => {
+          if (
+            response.code &&
+            (response.code === 400 || response.code === 403)
+          ) {
+            alert(response.message);
+          } else {
+            localStorage.setItem("token", response.token);
+            localStorage.setItem("id", response.id);
+
+            if (response.role === "atendente") {
+              routerHall();
+            } else if (response.role === "cozinheiro") {
+              routerKitchen();
+            } else {
+              alert("função/role desconhecida " + response.role);
+            }
+          }
+        })
+        .catch((error) => {
+          setMessageError("Erro na requisição. [" + error.message + "]");
+          console.log(messageError);
+        });
+    }
+  };
+
+  return { handleChange, handleSubmit, errors };
+};
+
+export default useForm;
--- src/services/api-auth.js
+++ src/services/api-auth.js
@@ -1,15 +1,28 @@
-export const register = async (email, password) => {
-  return await fetch('https://lab-api-bq.herokuapp.com/auth', {
+const baseURL = 'https://lab-api-bq.herokuapp.com';
+const headers = { "Content-Type": "application/json" }
+
+export const createUser= async (name, email, password, role) => {
+  return await fetch(`${baseURL}/users`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers,
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+      role: role,
+      restaurant: "Laricas Burguer"
+    }),
+  }).then(res => res.json())
+};
+
+
+export const LoginWithEmailPassword = async (email, password) => {
+    return await fetch(`${baseURL}/auth`, {
+    method: "POST",
+    headers,
     body: JSON.stringify({
       email: email,
       password: password
     })
-  }).then(res => res.json())
-    .catch((error) => {
-      console.log(error)
-    })
+  }).then(res => res.json());
 };
