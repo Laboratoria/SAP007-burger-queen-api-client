45add8b2f86317f9d90d1274c50e9ec66e607fb9
Layssa <oilayssaa@gmail.com>
2022-05-24 16:24:43 -0300

    review register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,19 +1,22 @@
 import UseForm from "./UseForm";
 import Input from "../../components/input/Input";
 import Button from "../../components/button/Button";
-import ErrorsMessage from "./register-valid";
+import ErrorsMessage from "./registerValidation";
 import { useState } from "react";
 import Modal from "../../components/modal/Modal";
-import { Link } from "react-router-dom";
-import { LoginRegisterParagraph } from "../../components/style-html-elements/elements-style";
+import { Link, Navigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import StyleBackgroundImg from "../../components/img-background/style-bg-img";
+import CircleOption from "../../components/circle-option/CircleOption";
+import { LoginRegisterParagraph } from "../../components/style-html-elements/elements-style";
+import { ModalOfMenuOptions } from "../../components/button/ButtonStyle";
 import Header from "../../components/header/Header";
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const { handleChange, handleSubmit, errors } = UseForm(ErrorsMessage);
   const [showPassword, setShowPassword] = useState(false);
-  const [showPassword2, setShowPassword2] = useState(false);
+  
   const [showModal, setShowModal] = useState(false);
 
   const handleClick = (e) => {
@@ -21,11 +24,9 @@ const Register = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleClick2 = (e) => {
-    e.preventDefault();
-    setShowPassword2(!showPassword2);
-  };
-
+  const navigate = useNavigate();
+  const handleLogin = () => navigate('/login')
+  
   const callModal = () => {
     setShowModal((prev) => !prev);
   };
@@ -45,7 +46,7 @@ const Register = () => {
           name="name"
           type="text"
           onChange={handleChange}
-          placeholder="Digite o seu nome"
+          placeholder="Digite o seu nome completo"
         />
         <LoginRegisterParagraph>
           {errors.name && errors.name}
@@ -85,49 +86,28 @@ const Register = () => {
           </LoginRegisterParagraph>
         </div>
 
-        <div>
-          <Input
-            label="Confirme sua senha:"
-            id="confirmPassword"
-            name="confirmPassword"
-            type={showPassword2 ? "type" : "password"}
-            onChange={handleChange}
-            placeholder="Confirme sua senha"
-          />
-          <span className="login-eye">
-            {showPassword2 ? (
-              <FaEye data-eye="2" size={20} onClick={handleClick2} />
-            ) : (
-              <FaEyeSlash data-eye="2" size={20} onClick={handleClick2} />
-            )}
-          </span>
-          <LoginRegisterParagraph>
-            {errors.confirmPassword && errors.confirmPassword}
-          </LoginRegisterParagraph>
-        </div>
-
-        <p>Selecione sua função:</p>
+        <p>Sua função:</p>
         <div>
           <div className="choose-role">
-            <Radio
+            <CircleOption
               className="option-role"
               text="Atendente"
               label="Atendente"
               id="role-hall"
               name="role"
-              type="radio"
+              type="checkbox"
               onChange={handleChange}
               value="atendente"
             />
           </div>
           <div className="choose-role">
-            <Radio
+            <CircleOption
               className="option-role"
               text="Cozinheiro"
               label="Cozinheiro"
               id="role-kitchen"
               name="role"
-              type="radio"
+              type="checkbox"
               onChange={handleChange}
               value="cozinheiro"
             />
@@ -137,14 +117,18 @@ const Register = () => {
             {errors.role && errors.role}
           </LoginRegisterParagraph>
         </div>
+        
+        <div className="buttons-register-return">
 
         <Button onClick={() => handleSubmit(callModal)}>Cadastre-se</Button>
+        <Button className='button' onClick={handleLogin}>Voltar</Button>
+        </div>
       </form>
       <Modal showModal={showModal} setShowModal={setShowModal}>
-        <p>Uhuuul, registro realizado com sucesso.</p>
+        <p>Cadastro realizado com sucesso!</p>
         <div style={{ textAlign: "center" }}>
           <Link to="/login">
-            <>Faça Login aqui</>
+            <ModalOfMenuOptions>Fazer Login</ModalOfMenuOptions>
           </Link>
         </div>
       </Modal>
@@ -153,3 +137,4 @@ const Register = () => {
 };
 
 export default Register;
+
