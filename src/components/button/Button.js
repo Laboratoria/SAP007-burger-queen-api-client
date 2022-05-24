f830df16e78721f895de70f582bbdc79c91223a6
Layssa <oilayssaa@gmail.com>
2022-05-24 16:01:39 -0300

    estilos dos botoes

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react'
+import { Container } from './ButtonStyle';
 
-const Button = ({ children, ...props }) => {
-  const variant = props.className ? props.className : "";
-  const btnComponent = `btn-class ${variant}`;
+const Button = ({children, ...props}) => {
+    const variant = props.className ? props.className : "";
+    const btnComponent = `btn-class ${variant}`;
 
-  return (
-    <Container {...props} className={btnComponent}>
-      {children}
-    </Container>
-  );
+    return (
+        <Container {...props} className={btnComponent}>
+           {children}
+        </Container>
+    )
 };
 
-export default Button;
+export default Button
+
---
+++ src/components/button/ButtonStyle.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+// btn login and register
+export const Container = styled.button`
+  
+  font-size: 1.2rem;
+  cursor: pointer;
+  border-radius: 0.4rem;
+  height: 3.5rem;
+  width: 40%;
+  margin-top: 33px;
+  border: none;
+  background: #FF5B27;
+  
+`;
+
+export const ButtonsHall = styled.button `
+  width: 14rem;
+  background: #531CB3;
+  border-radius: 0.4rem;
+  height: 3.5rem;
+  font-size: 1.2rem;
+  cursor: pointer;
+  color: white;
+  border: none;
+  margin-left: 10px;
+`;
+
+
+// btn finalizar pedido - was secondaryButton
+export const BtnKitchenFinishOrder = styled(Container)`
+  background: green;
+  color:  white;
+  box-shadow: none;
+  border: none;
+
+  &:hover {
+    background: transparent;
+    border: none;
+    color: green;
+    box-shadow: 0 0 30px 10px #531CB3;
+  }
+`;
+
+export const CancelOrder = styled(BtnKitchenFinishOrder)`
+  background: #FF5B27;
+  color: var(--white);
+  border: none;
+
+  &:hover {
+    box-shadow: 0 0 30px 10px #531CB3;
+    border: 2px solid;
+    color: #9d0505;
+  }
+`;
+// was green btn
+export const BtnSendOrderToKitchenAndPrepareIt = styled(BtnKitchenFinishOrder)`
+  background: green;
+  color: white;
+  border: none;
+
+  &:hover {
+    box-shadow: 0 0 30px 10px #531CB3;
+    
+    color: #138b2f;
+  }
+`;
+
+export const BtnReadyAndDeliveredOrders = styled(Container)`
+  background: #531cb3;
+  color: #f7f7ff;
+  border: none;
+
+`;
+export const ModalOfMenuOptions = styled(Container)`
+  background: var(--dark-orange);
+  color: red;
+  box-shadow: none;
+  border: none;
+
+  &:hover {
+    background: var(--light-orange);
+    border: none;
+    color: var(--black);
+    box-shadow: none;
+  }
+`;
