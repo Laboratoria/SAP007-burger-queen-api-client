97ddb90594f89bdf880d45d8f3606f9a16da31b0
Layssa <oilayssaa@gmail.com>
2022-05-24 16:06:32 -0300

    estilo header welcome

---
+++ src/components/header/headerStyleWelcome.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+// import img from '../../images/welcome.gif';
+
+export const ContainerWelcome = styled.header `
+    display: flex;
+    align-items: center;
+    display: grid;
+    border-bottom: 1px solid #474747;
+    padding-bottom: 1em;  
+    background-color: black;  
+   
+
+    p {
+        argin-left: 45px;
+    }
+    
+    .logo {
+        width: 100px;
+        margin-top: 0.70em;
+        margin-left: 128px;
+                       
+    }
+
+` 
