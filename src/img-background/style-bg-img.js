978ed6929cb547ef0b6116b1b35a59a0489358c1
Layssa <oilayssaa@gmail.com>
2022-05-24 16:08:05 -0300

    estilo da imagem de fundo com mediaquery

---
+++ src/img-background/style-bg-img.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import img from '../../images/bq3.jpeg';
+import imgMediaQuery from '../../images/bq3.jpeg';
+
+const StyleBackgroundImg = styled.div `
+   
+   background-image: url(${img});
+    background-repeat: no-repeat;
+    width: 100%;
+    min-height: 100vh;
+    top:0;
+    z-index: -10;
+    position: fixed;
+    background-size: cover;
+    background-position: center;
+    filter: blur(4px);
+
+    @media screen and (min-width: 768px) {
+        background-image: url(${imgMediaQuery});
+        
+    }
+
+`
+export default StyleBackgroundImg
+
