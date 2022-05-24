25ffd23735a5d7e75054e0a99ceb589ddc592278
Layssa <oilayssaa@gmail.com>
2022-05-24 16:26:07 -0300

    mensagens de erro

--- src/pages/register/register-valid.js
+++ src/pages/register/register-valid.js
@@ -1,35 +1,34 @@
 export const ErrorsMessage = (values) => {
   let errors = {};
-  
+
   if (values.name.length < 3) {
-    errors.name = 'Preencha seu nome';
-  } 
-  
+    errors.name = "Digite seu nome";
+  }
+
   if (!values.email) {
-    errors.email = 'Preencha seu email.';
+    errors.email = "Digite seu email";
   } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-    errors.email = 'Ops, email inválido.';
+    errors.email = "Opa, email inválido ";
   }
 
   if (!values.password) {
-    errors.password = 'Preencha sua senha'
+    errors.password = "Digite uma senha";
   } else if (values.password.length < 6) {
-    errors.password = 'Eita, senha curta. Pelo menos 5 caracteres.';
-  }
-
-  if (!values.confirmPassword) {
-    errors.confirmPassword = 'Por favor, verifique sua senha';
-  } else if (values.confirmPassword !== values.password) {
-    errors.confirmPassword = 'Oh, no! As senhas não coincidem';
+    errors.password = "Opa, a senha deve ser maior que 5 caracteres";
   }
 
   if (!values.role) {
-    errors.role = 'Colaboradore, selecione sua função';
-  } 
+    errors.role = "Selecione sua função";
+  }
 
-  errors.ok = (!errors.name && !errors.email && !errors.password && !errors.confirmPassword && !errors.role);
+  errors.ok =
+    !errors.name &&
+    !errors.email &&
+    !errors.password &&
+    !errors.confirmPassword &&
+    !errors.role;
 
   return errors;
-}
+};
 
 export default ErrorsMessage;
