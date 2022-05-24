6095d0ff42b5cd5ab73a0426bc4df4eb1435ccab
Layssa <oilayssaa@gmail.com>
2022-05-24 17:45:08 -0300

    review imports

--- src/pages/Hall.jsx
+++ src/pages/Hall.jsx
@@ -8,10 +8,7 @@ import {
   DivProduct,
 } from "../components/modal-menu/body/MenuCardModalStyle.js";
 import { MdDelete } from "react-icons/md";
-import {
-  DefaultTitle,
-  DefaultParagraph,
-} from "../components/style-html-elements/elements-style.js";
+import { DefaultParagraph } from "../components/style-html-elements/elements-style.js";
 import Header from "../components/header/Header.js";
 import Modal from "../components/modal/Modal.js";
 import {
@@ -28,3 +25,26 @@ import burguerDuploImg from "../images/bg-duplo.png";
 import drinkImg from "../images/drink.png";
 import friesImg from "../images/fries.png";
 import StyleBackgroundImg from "../components/img-background/style-bg-img";
+
+const Hall = () => {
+  const [showBreakfast, setShowBreakfast] = useState(false);
+  const [showHamburguer, setShowHamburguer] = useState(false);
+  const [showHamburguerDuplo, setShowHamburguerDuplo] = useState(false);
+  const [showDrink, setShowDrink] = useState(false);
+  const [showSide, setShowSide] = useState(false);
+  const [nameClient, setNameClient] = useState("");
+  const [table, setTable] = useState("");
+  const [order, setOrder] = useState([]);
+  const [allValue, setAllValue] = useState("");
+  const [showResume, setShowResume] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showCancelModal, setShowCancelModal] = useState(false);
+  const [menu, setMenu] = useState([]);
+  const [readyOrder, setReadyOrder] = useState([]);
+
+  useEffect(() => {
+    RenderAllProducts()
+      .then((json) => setMenu(json))
+      .catch((error) => {
+        alert("Erro na requisição. [" + error.message + "]");
+      });
