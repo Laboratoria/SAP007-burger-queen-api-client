473937f2d342d0eb013259f62d6998a649fafef2
Marione-Pereira <mari.tainara@gmail.com>
2022-05-24 10:53:48 -0300

    rotas privadas adicionadas
    [200~
    [200~
    Co-authored-by: Marione-Tainara <mari.tainara@gmail.com>
    Co-authored-by: moreirathais <thais_mjs@hotmail.com>

---
+++ src/services/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import {  Navigate} from "react-router-dom";
+
+
+export const PrivateRoute = ({ children, redirectTo }) => {
+  const isAuthenticated = localStorage.getItem("token") !== null;
+  console.log("isAuth: ", isAuthenticated);
+  return isAuthenticated ? children : <Navigate to={redirectTo} />;
+};
+
+
+
