2b383e44f2e0b399f904e27120403b0c0cb88e99
Camila Aguiar <camilaguiar73@gmail.com>
2022-05-23 21:06:40 -0300

    corrigindo erro do react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+
+const App= () => {
+  return ( 
+    <h1> Hello World</h1>
+  )
 }
 
 export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1> Hello World! </h1>
+//     </div>
+//   );
+// }
+
+// export default App;
