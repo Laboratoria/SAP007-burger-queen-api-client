bd9b83878a711487d3af2c1fbbdf6998fe64b498
Layssa <oilayssaa@gmail.com>
2022-05-25 17:54:54 -0300

    add produtos a comanda

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Login from './pages/Login/Login';
+import Login from './pages/login/login';
 import Register from './pages/register/Register';
 
 // import PrivateRoute from './privateRoute.js';
