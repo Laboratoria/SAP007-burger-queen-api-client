8454d7d5bf7880c0c7d8ee8a9b68af93c63e80d4
Layssa <oilayssaa@gmail.com>
2022-05-26 15:36:49 -0300

    formatando

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,28 +1,26 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Login from './pages/login/login';
-import Register from './pages/register/Register';
-import Hall from './pages/Hall';
-import Kitchen from './pages/Kitchen';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/login";
+import Register from "./pages/register/Register";
+import Hall from "./pages/Hall";
+import Kitchen from "./pages/Kitchen";
 
 // import PrivateRoute from './privateRoute.js';
 // import { render } from "react-dom";
-
 
 const AllRoutesApp = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/hall' element={<Hall />} />
-          <Route path='/kitchen' element={<Kitchen />} />
-         
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/hall" element={<Hall />} />
+          <Route path="/kitchen" element={<Kitchen />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default AllRoutesApp;
