02251b9f56f2dbf5bec252d97cf64ddc015f08d1
Marione-Pereira <mari.tainara@gmail.com>
2022-05-26 10:04:49 -0300

    função de adicionar produtos no pedido
    Co-authored-by: Marione-Tainara <mari.tainara@gmail.com>
    Co-authored-by: moreirathais <thais_mjs@hotmail.com>

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,23 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../../global/GlobalContext'
+import Button from '../Button'
 import styles from "./style.module.css"
+
 // puxar a lista de produtos
-function Card ({product,name, price,flavor}){
+function Card ({product}){
+  const {states, setter} = useContext(GlobalContext)
+  const setPedidos = setter.setPedidos
+  const pedidos= states.pedidos
+//  console.log(pedidos)
+
+ const addProduto =(product)=>{
+    const novaLista =[pedidos,product]
+    setPedidos(novaLista)
+    console.log(pedidos)
+  }
+  
   return(
+    <div>
       <ul className={styles.DivCard}>
          <li>
           <p>{product.name}</p>
@@ -10,9 +26,10 @@ function Card ({product,name, price,flavor}){
           <p>R${(product.price).toFixed(2)}</p>
           </li>
           {product.flavor ? <li><p>{product.flavor}</p></li>:null}
-          
-         
       </ul>
+      <Button  type="button" onClick={()=>addProduto(product)} children="Adicionar"/>
+      
+      </div>
   )
 }
 export default Card
