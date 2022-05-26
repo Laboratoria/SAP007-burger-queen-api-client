4533a11e9292bcc560a96b228e5a9540a231aa9a
Layssa <oilayssaa@gmail.com>
2022-05-26 16:24:16 -0300

    rotas pedidos prontos e entregues

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/login/login";
 import Register from "./pages/register/Register";
 import Hall from "./pages/Hall";
 import Kitchen from "./pages/Kitchen";
+import ReadyOrders from './pages/ReadyOrders'
+import OrdersDelivered from './pages/OrdersDelivered'
 
 // import PrivateRoute from './privateRoute.js';
 // import { render } from "react-dom";
@@ -17,6 +19,8 @@ const AllRoutesApp = () => {
           <Route path="/register" element={<Register />} />
           <Route path="/hall" element={<Hall />} />
           <Route path="/kitchen" element={<Kitchen />} />
+          <Route path='/readyorders' element={<ReadyOrders />} />
+          <Route path='/ordersdelivered' element={<OrdersDelivered />} />
         </Routes>
       </div>
     </Router>
