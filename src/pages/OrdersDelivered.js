fdc8706858181faf91f7f41a5f222a91fd968628
Layssa <oilayssaa@gmail.com>
2022-05-26 15:53:22 -0300

    template vista da cozinheira

--- src/pages/OrdersDelivered.js
+++ src/pages/OrdersDelivered.js
@@ -21,4 +21,37 @@ const OrdersDelivered = () => {
 
     }, [])
 
-    
+    return (
+        <>
+            <StyleBackgroundImg />
+            <Header showLogOut={true} />                    
+           
+            <div className="container">
+                <DefaultTitle>Pedidos Entregues</DefaultTitle>
+                <Link to="/hall"><div style={{color: '#ce8e43', textAlign: "left"}} > Voltar</div></Link>
+            </div>
+                
+            {orders.map((item) => (
+                <div className="container" key={item.id}>
+                    <BackgroundCard >
+                        <DivProductKitchen>
+                            <OrderSummaryH2>Status: {item.status}</OrderSummaryH2>
+                            <DefaultParagraph>Nome: {item.client_name}</DefaultParagraph>
+                            <DefaultParagraph>Mesa: {item.table}</DefaultParagraph>
+                            <div className="container">
+                            {item.Products.map((product) =>
+                                <span key={product.id}>
+                                <p>{product.qtd} {product.name} {product.flavor} {product.complement}</p>                                   
+                                </span>                                   
+                               
+                            )}
+                            </div>
+                        </DivProductKitchen>
+                    </BackgroundCard>
+                </div>                
+            ))}
+        </>
+    )
+}
+
+export default OrdersDelivered;
