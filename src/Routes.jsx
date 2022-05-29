5ca39d1979215304d6f3b9cf0fb9463f9f4ae263
Layssa <oilayssaa@gmail.com>
2022-05-28 21:03:48 -0300

    formatando

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,13 +3,8 @@ import Login from "./pages/login/login";
 import Register from "./pages/register/Register";
 import Hall from "./pages/Hall";
 import Kitchen from "./pages/Kitchen";
-import ReadyOrders from './pages/ReadyOrders'
-import OrdersDelivered from './pages/OrdersDelivered'
-
-// import PrivateRoute from './privateRoute.js';
-// import { render } from "react-dom";
-
-
+import ReadyOrders from "./pages/ReadyOrders";
+import OrdersDelivered from "./pages/OrdersDelivered";
 import PrivateRoute from "./privateRoute";
 
 const AllRoutesApp = () => {
@@ -17,26 +12,46 @@ const AllRoutesApp = () => {
     <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/hall' element={<PrivateRoute redirectTo="/">
-            <Hall /> 
-          </PrivateRoute>} />
-          <Route path='/kitchen' element={<PrivateRoute redirectTo="/">
-            <Kitchen /> 
-          </PrivateRoute>} />
-          <Route path='/readyorders' element={<PrivateRoute redirectTo="/">
-            <ReadyOrders />
-          </PrivateRoute>} />
-          <Route path='/ordersdelivered' element={<PrivateRoute redirectTo="/">
-            <OrdersDelivered />
-          </PrivateRoute>} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/hall"
+            element={
+              <PrivateRoute redirectTo="/">
+                <Hall />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/kitchen"
+            element={
+              <PrivateRoute redirectTo="/">
+                <Kitchen />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/readyorders"
+            element={
+              <PrivateRoute redirectTo="/">
+                <ReadyOrders />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/ordersdelivered"
+            element={
+              <PrivateRoute redirectTo="/">
+                <OrdersDelivered />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default AllRoutesApp;
 
