8d5d9c58a45215e873e86ab1ff0fdd27763c7266
moreirathais <thais_mjs@hotmail.com>
2022-06-08 18:19:37 -0300

    Fazendo resquisição na api ref.status

--- src/pages/kitchen.jsx
+++ src/pages/kitchen.jsx
@@ -1,18 +1,17 @@
-import { allOrders } from "../services/api";
+import { allOrders, updateOrder } from "../services/api";
 import jake from "../img/jake.jpg";
 import TemplateKitchen from "../components/TemplateKitchen";
 import Header from "../components/Header";
 import { preparationTime } from "../components/Time";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 function Kitchen() {
   const navigate = useNavigate();
   const [order, setOrder] = useState([]);
-  const [orderByStatus, setOrderByStatus] = useState([]);
-  console.log(orderByStatus);
- 
+
   function handleLogout() {
     localStorage.removeItem("token");
     navigate("/login");
@@ -28,15 +27,20 @@ function Kitchen() {
     console.log(order);
   }, [order]);
 
-  const filterByStatus = useCallback(
-    (status) => {
-      const results = order.filter((item) => item.status === status);
-      setOrderByStatus(results);
-      console.log(filterByStatus());
-    },
-    [order]
-  );
-
+  const orderByStatus = (item, event) => {
+    updateOrder(item.id, event.target.value).then((response) => {
+      if (response.status === 200) {
+        const results = order.map((result) => {
+          if (result.id === item.id) {
+            result.status = event.target.value;
+          }
+          return result;
+        });
+        setOrder(results);
+      }
+    });
+  };
+  
   return (
     <div>
       <Header
@@ -47,8 +51,9 @@ function Kitchen() {
       <section className="sectionTemplateKitchen">
         {order.map((item) => {
           const orderReady = item.status === "ready";
-            return (<TemplateKitchen 
-              key={item.id} 
+          return (
+            <TemplateKitchen
+              key={item.id}
               id={item.id}
               client={item.client_name}
               table={item.table}
@@ -56,18 +61,26 @@ function Kitchen() {
               createdAt={item.createdAt}
               updatedAt={item.updatedAt}
               processedAt={orderReady ? item.processedAt : ""}
-              preparedAt={orderReady ? preparationTime(item.processedAt, item.createdAt) : "" }
-              products = {item.Products}
+              {...item.status === "pending" ?
+              <Button onClick={(event) => orderByStatus(item, event)}  value="preparing"children="Preparando"/> :
+              item.status === "preparing" &&
+              <Button onClick={(event) => orderByStatus(item, event)}  value="ready"children="Pronto"/> 
+            }
+              preparedAt={
+                orderReady
+                  ? preparationTime(item.processedAt, item.createdAt)
+                  : ""
+              }
+              products={item.Products}
               />
-            )
-        }
-      )}
+              );
+            })}
       </section>
       <div className="logout" onClick={handleLogout}>
         <FaSignInAlt size="26px" margin-rigth="0px" />
       </div>
     </div>
-  ); 
+  );
 }
 
 export default Kitchen;
