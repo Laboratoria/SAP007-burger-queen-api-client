4018942b28b769b2224884d9d99ba71e0eb77665
Marione-Pereira <mari.tainara@gmail.com>
2022-06-13 18:08:32 -0300

    teste card ok
    Co-authored-by: Marione-Tainara <mari.tainara@gmail.com>
    Co-authored-by: moreirathais <thais_mjs@hotmail.com>

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,19 @@
 import styles from "./style.module.css"
-import {FaRegPlusSquare} from "react-icons/fa"
+import { FaRegPlusSquare } from "react-icons/fa"
 
-function Card ({product, onClick}){
-  return(
+function Card({ product, onClick }) {
+  return (
     <div className={styles.DivCard}>
-         <ul className={styles.UlCard}>
-         <li>  
+      <ul className={styles.UlCard}>
+        <li>
           <p className={styles.id}>{product.id}</p>
-          <p>{product.name}</p>      
+          <p>{product.name}</p>
           <p>R${(product.price).toFixed(2)}</p>
-          {product.flavor ? <li><p>{product.flavor}</p></li>:null}     
-          <p>{product.qtd}</p>
-          </li>
-          <FaRegPlusSquare onClick={onClick} size="20px" className='sum'/>
-      </ul>    
-      </div>
+          {product.flavor ? <p>{product.flavor}</p> : null}
+        </li>
+        <FaRegPlusSquare data-testid="icon" onClick={onClick} size="20px" className='sum' />
+      </ul>
+    </div>
   )
 }
 export default Card;
