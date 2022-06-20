e6cf5519098d7d82b8feccb9147642d0f8ecc432
Keila Hadama <hadamakeila@gmail.com>
2022-06-20 18:29:50 -0300

    correcao conteudo da pag salao, qnd sai de menu pedidos

--- src/components/orders/orders.test.js
+++ src/components/orders/orders.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { getAllProducts } from "../../services/products";
+
 import Order from "./index";
 
 describe("Tests for Order component", () => {
