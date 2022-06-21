4badcb8541996cc16202cea45e36fc1fe18ad59b
Leticia Antunes Almeida <leticiaantunes95@outlook.com>
2022-06-21 15:03:29 -0300

    Testes Unitários Finais

--- src/components/orders/orders.test.js
+++ src/components/orders/orders.test.js
@@ -3,13 +3,13 @@ import { render, screen } from "@testing-library/react";
 import Order from "./index";
 
 describe("Tests for Order component", () => {
-  let order = [{
+  const order = {
     status: "Pronto",
     id: 5829,
     table: 5,
     client_name: "Maria",
     createdAt: "2022-06-17T20:50:58.066Z",
-    // processedAt: "20/06/2022 13:15",
+    // processedAt: "13/06/2022 20:22",
     updatedAt: "2022-06-20T17:05:03.338Z",
     Products: [{
         name: "hamburguer",
@@ -17,13 +17,15 @@ describe("Tests for Order component", () => {
         complement: "ovo",
         qtd: 1,
     }],
-  }]
+  }
 
   it("should render the order with table number", () => {
     render(<Order order={order} />);
-    let table = screen.getByNumber(order[0].table);
-
+    let name = screen.getByText("Cliente: Maria");
+    let table = screen.getByText("Mesa: 5");
+ 
+    expect(name).toBeInTheDocument();
     expect(table).toBeInTheDocument();
-    expect(table).toBeEqual(5);
+    expect(name).toHaveTextContent("Cliente: Maria");
   });
 });
