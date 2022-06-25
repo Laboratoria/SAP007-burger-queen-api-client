660613f0424142dc521f7537190f46fd8a4a8f20
Natalie I. S. Santos <natalie_ingrid@hotmail.com>
2022-06-25 20:23:17 -0300

    Acrescimo do uso de neolinguagem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             <h1 className={styles.title}>Bem-vindo ao Burger Heroes</h1>
             <Logo customClass='logo' />
             <p className={styles.about}>Sabemos o quanto salvar o mundo é importante!
-                Por isso este app optimiza o atendimento dos super heróis do nosso planeta.
+                Por isso este app otimiza o atendimento dos super heróis do nosso planeta.
                 Você pode estar se perguntando se este restaurante é somente para o Batman ou Capitão America.
                 De fato nossos heróis da Marvel e DC também são apaixonados por essa hamburgueria,
                 mas entendemos que qualquer pessoa que consuma produtos com embalagens e utensílios
