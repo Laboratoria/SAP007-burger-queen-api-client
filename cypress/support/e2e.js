84795b89b8ed0112ea3970554296ced25cdd4128
Marione-Pereira <mari.tainara@gmail.com>
2023-02-13 20:51:02 -0300

    removendo testes visuais

--- cypress/support/e2e.js
+++
@@ -1,6 +0,0 @@
-import "./commands";
-import "@argos-ci/cypress/support";
-Cypress.on("uncaught:exception", (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false;
-});
